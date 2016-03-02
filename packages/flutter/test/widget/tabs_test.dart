35b1b3a82326cfa450e33359f3ad2c3df6a7bcd3
Hans Muller <hansmuller@chromium.org>
2016-03-02 14:08:19 -0800

    Restored tab scrolling

--- packages/flutter/test/widget/tabs_test.dart
+++ packages/flutter/test/widget/tabs_test.dart
@@ -7,12 +7,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:test/test.dart';
 
-Widget buildFrame({ List<String> tabs, String value, bool isScrollable: false }) {
+Widget buildFrame({ List<String> tabs, String value, bool isScrollable: false, Key tabBarKey }) {
   return new Material(
     child: new TabBarSelection<String>(
       value: value,
       values: tabs,
       child: new TabBar<String>(
+        key: tabBarKey,
         labels: new Map<String, TabLabel>.fromIterable(tabs, value: (String tab) => new TabLabel(text: tab)),
         isScrollable: isScrollable
       )
@@ -98,4 +99,27 @@ void main() {
       expect(selection.value, equals('A'));
     });
   });
+
+  test('Scrollable TabBar tap centers selected tab', () {
+    testWidgets((WidgetTester tester) {
+      List<String> tabs = <String>['AAAAAA', 'BBBBBB', 'CCCCCC', 'DDDDDD', 'EEEEEE', 'FFFFFF', 'GGGGGG', 'HHHHHH', 'IIIIII', 'JJJJJJ', 'KKKKKK', 'LLLLLL'];
+      Key tabBarKey = new Key('TabBar');
+      tester.pumpWidget(buildFrame(tabs: tabs, value: 'AAAAAA', isScrollable: true, tabBarKey: tabBarKey));
+      TabBarSelectionState<String> selection = TabBarSelection.of(tester.findText('AAAAAA'));
+      expect(selection, isNotNull);
+      expect(selection.value, equals('AAAAAA'));
+
+      expect(tester.getSize(tester.findElementByKey(tabBarKey)).width, equals(800.0));
+      // The center of the FFFFFF item is to the right of the TabBar's center
+      expect(tester.getCenter(tester.findText('FFFFFF')).x, greaterThan(401.0));
+
+      tester.tap(tester.findText('FFFFFF'));
+      tester.pump();
+      tester.pump(const Duration(seconds: 1)); // finish the scroll animation
+      expect(selection.value, equals('FFFFFF'));
+      // The center of the FFFFFF item is now at the TabBar's center
+      expect(tester.getCenter(tester.findText('FFFFFF')).x, closeTo(400.0, 1.0));
+    });
+  });
+
 }
